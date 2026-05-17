fd347bf7f7e073e8e0ef6162cde41aaded0d6eea
RifleTVPT <andreanatoliycorreia@gmail.com>
2026-05-17 19:39:37 +0100

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,58 @@
 import 'package:flutter/material.dart';
-import 'services/rotas.dart'; // Importa o ficheiro das rotas
+import 'package:firebase_core/firebase_core.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:go_router/go_router.dart';
+import 'services/rotas.dart';
+import 'services/notificacoes.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  try {
+    await Firebase.initializeApp();
+  } catch (e) {
+    print("Erro ao iniciar Firebase: $e");
+  }
+
+  // login
+  // Lemos a memória do telemóvel
+  final prefs = await SharedPreferences.getInstance();
+  // Vemos se a variável 'isLogged' é true. Se não existir, assume false.
+  final bool isLogged = prefs.getBool('isLogged') ?? false;
+  // Decide a rota, se isLogged for true vai para dashboard, senão vai para login
+  final String rotaInicial = isLogged ? '/dashboard' : '/';
+
+  // Criamos o router com a rota decidida
+  final GoRouter router = criarRouter(rotaInicial);
+
+  try {
+    final navigatorKey = router.routerDelegate.navigatorKey;
+    await Notificacoes().inicializar(navigatorKey);
+    print("Firebase e Notificações iniciados com sucesso!");
+  } catch (e) {
+    print("Erro ao iniciar Notificações: $e");
+  }
+
+  // Passamos o router para a nossa App
+  runApp(MyApp(router: router));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final GoRouter router; // Recebe o router configurado no main
+
+  const MyApp({super.key, required this.router});
 
   @override
   Widget build(BuildContext context) {
-    // Usamos MaterialApp.router em vez de MaterialApp normal
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: 'Softinsa Badges',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF34659D)),
         useMaterial3: true,
       ),
-      // Aqui ligamos as configurações do GoRouter
-      routerConfig: rotas,
+      // Ligamos o nosso router dinâmico
+      routerConfig: router,
     );
   }
 }
